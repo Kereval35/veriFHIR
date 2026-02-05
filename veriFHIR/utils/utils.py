c7f9c660309fc211a4c5a7e7667814ab0a261b6d
sly-kereval <125375447+sly-kereval@users.noreply.github.com>
2026-02-05 10:19:38 +0100

    Utils typing

--- veriFHIR/utils/utils.py
+++ veriFHIR/utils/utils.py
@@ -5,8 +5,8 @@ from typing import Tuple
 
 
 def extract_zip(zip_path: str) -> Tuple[TemporaryDirectory, Path]:
-    temp_dir = TemporaryDirectory()
-    temp_path = Path(temp_dir.name)
+    temp_dir: TemporaryDirectory = TemporaryDirectory()
+    temp_path: Path = Path(temp_dir.name)
     with zipfile.ZipFile(zip_path, 'r') as zip_ref:
         zip_ref.extractall(temp_path)
     return temp_dir, temp_path
